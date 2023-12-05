af99a631aad2807fd998ee0fe1ce3fd2254b6571
cristeacrs <cristea.cristi@yahoo.com>
2023-12-05 21:54:52 +0200

    [FEAT] get advice app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+// import {AcmeLogo} from "./AcmeLogo.jsx";
 
+import { useState, useEffect } from "react";
+// import {NextUIProvider} from "@nextui-org/react";
+
+import "./App.css";
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [advice, setAdvice] = useState("Initial advice");
+
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    console.log("de cate ori sunt folosit ", advice);
+  }, [advice]);
+
+  async function getAdvice() {
+    setLoading(true);
+    try {
+      const timeStamp = Date.now();
+      const res = await fetch("https://api.adviceslip.com/advice?" + timeStamp);
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      const currentHistory = [...history];
+      currentHistory.push(data.slip.advice);
+      setHistory(currentHistory);
+    } catch (error) {
+      console.error("Eroare prinsa de cristi", error);
+
+      setAdvice(error.message);
+    }
+    setLoading(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="header">Welcome to advices</h1>
+
+      <button
+        type="button"
+        className="btn btn-success button-advice"
+        onClick={getAdvice}
+        disabled={loading}
+      >
+        Get advice
+      </button>
+      {history.map((text) => (
+        <div key={text}>{text}</div>
+      ))}
+      <h3 className="text-center">{advice}</h3>
     </div>
   );
 }
